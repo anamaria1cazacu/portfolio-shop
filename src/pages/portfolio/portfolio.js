c037795ac2cab4b54df411071e4db73656e3fa32
AnneRPC <150238411+anneRPC@users.noreply.github.com>
2024-06-01 19:50:25 +0300

    portfolio

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -1,7 +1,13 @@
 import "./portfolio.css";
+import Image from "../../components/image/image";
 
 function Portfilio() {
-  return <div></div>;
+  return (
+    <div>
+      <p>Small collection of art I made between 2021 and present.</p>
+      <Image />
+    </div>
+  );
 }
 
 export default Portfilio;
