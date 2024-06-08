aa5191f97c30f8b5e566e78ecb46bd4f0c9c071c
AnneRPC <150238411+anneRPC@users.noreply.github.com>
2024-06-08 11:16:19 +0300

    added img

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,7 +1,28 @@
+import PropTypes from "prop-types";
 import "./image.css";
 
-function Image() {
-  return <div></div>;
-}
+const Image = (props) => {
+  return (
+    <div className="container">
+      <div className="preview">
+        <img alt={"PreviewPhoto"} src={props.link} />
+      </div>
+      <div className="name">
+        <span>Title:</span>
+        <p>{props.name}</p>
+      </div>
+      <div className="tags">
+        <span>TAGS:</span>
+        <p>{props.tags}</p>
+      </div>
+    </div>
+  );
+};
+
+Image.propTypes = {
+  link: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+};
 
 export default Image;
